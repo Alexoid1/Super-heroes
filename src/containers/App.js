6813daac1aeb64be2daced6b064eceb6d810e94a
alex <alex_zam@hotmail.es>
2021-08-08 02:54:32 -0500

    run deploy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HeroesCatalogue from './HeroesCatalogue';
+import HeroInfo from '../components/HeroInfo';
+
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="animated-background" data-testid="website_name">
-        <Routes />
+        <Switch>
+          <Route exact path="/" component={HeroesCatalogue} />
+          <Route exact path="/hero/:category/:id" component={HeroInfo} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
