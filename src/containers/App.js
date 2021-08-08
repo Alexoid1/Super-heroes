5fc44d8580f790eca8a2b13bcd4419fb0b73f0e0
alex <alex_zam@hotmail.es>
2021-08-08 03:01:23 -0500

    run deploy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HeroesCatalogue from './HeroesCatalogue';
+import { Provider } from 'react-redux';
+import store from '../reducers/index';
 import HeroInfo from '../components/HeroInfo';
 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="animated-background" data-testid="website_name">
-        <Switch>
-          <Route exact path="/" component={HeroesCatalogue} />
-          <Route exact path="/hero/:category/:id" component={HeroInfo} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="animated-background" data-testid="website_name">
+          <Switch>
+            <Route exact path="/" component={HeroesCatalogue} />
+            <Route exact path="/hero/:category/:id" component={HeroInfo} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Provider>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './containers/App';
-import store from './reducers/index';
 import './index.css';
 
 ReactDOM.render(
-
-  <Provider store={store}>
     <App />
-  </Provider>,
-
+  ,
   document.getElementById('root'),
 );
