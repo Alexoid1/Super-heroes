e958345c6a02d367e89cd232f9e615fc620792d9
alex <alex_zam@hotmail.es>
2021-08-09 14:25:01 -0500

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+
+
+  return (
+    <nav className="navb"><button type="button">Create Hero</button></nav>
+
+  );
+}
+
+
+
+export default Navbar;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import Navbar from '../components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="animated-background" data-testid="website_name">
+        <Navbar/>
         <Routes />
       </div>
     </BrowserRouter>
