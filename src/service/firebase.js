0283bf28e5cabc1cdab406a69b67ca1b02541ac7
alex <alex_zam@hotmail.es>
2021-08-24 12:14:27 -0500

    add google analytics

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
+import 'firebase/analytics';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -12,13 +13,17 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 export const projectStorage = firebase.storage();
 export const projectFirestore = firebase.firestore();
 export const authh = firebase.auth();
 export const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+export const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
 
 export default firebase;
